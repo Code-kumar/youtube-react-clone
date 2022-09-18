0921aead897245b3ba42867554c4be85a4fb82e7
Cod-kumar <1606.nk@gmail.com>
2022-09-18 19:45:43 +0530

    v1.0.0

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -10,7 +10,7 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 const VideoDetail = () =>{
 
     const [videoDetail, setVideoDetail] = useState(null);
-    const [videos, setVideos] = useState(null); 
+    const [videos, setVideos] = useState([]); 
 
     const { id } = useParams();
     
@@ -20,7 +20,7 @@ const VideoDetail = () =>{
 
         fetchFromAPI(`search?part=sinppet&relatedToVideoId=${id}&type=video`)
         .then((data) => setVideos(data.items));
-    }, [id])
+    }, [id]);
 
     if(!videoDetail?.snippet) return 'Loading...';
     const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount }}= videoDetail;
